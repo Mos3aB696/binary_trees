f3f4ba050cab0823e0e4f1aca6ef6ea981fb7dc0
Amirametwally <amira.metwally56@gmail.com>
2024-01-04 20:16:44 +0200

    9. Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+
+/**
+ * height_recursion - function that measures the
+ * height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: the height of tree or 0
+ */
+size_t height_recursion(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_subtree = 0;
+	size_t right_subtree = 0;
+
+	left_subtree = height_recursion(tree->left);
+	right_subtree = height_recursion(tree->right);
+
+	if (right_subtree > left_subtree)
+	{
+		return (right_subtree + 1);
+	}
+	else
+	{
+		return (left_subtree + 1);
+	}
+}
+
+/**
+ * binary_tree_height - measures the
+ * height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: the height of tree or 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (height_recursion(tree) - 1);
+}
