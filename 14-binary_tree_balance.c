f00a0bc1182ebb703067d0e7651a5110a4eb25af
Amirametwally <amira.metwally56@gmail.com>
2024-01-04 23:14:45 +0200

    14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -55,5 +55,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->right) - binary_tree_height(tree->left));
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
